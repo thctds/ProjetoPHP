e28ff0f351c0ac8907cddae90c713d18ceece6f1
thctds <tiagoh.coelho@hotmail.com>
2017-12-01 01:13:48 -0300

    Complemento do commit final
    Arquivos modificados em sala para otimização do sistema

---
+++ bank.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 01-Dez-2017 às 01:11
+-- Versão do servidor: 10.1.28-MariaDB
+-- PHP Version: 7.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `aluno`
+--
+
+CREATE TABLE `aluno` (
+  `id_aluno` int(11) NOT NULL,
+  `login` varchar(50) DEFAULT NULL,
+  `senha` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `aluno`
+--
+ALTER TABLE `aluno`
+  ADD PRIMARY KEY (`id_aluno`),
+  ADD UNIQUE KEY `login` (`login`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `aluno`
+--
+ALTER TABLE `aluno`
+  MODIFY `id_aluno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- selectAluno.php
+++ selectAluno.php
@@ -12,20 +12,20 @@
 	<link href='CHECKBK0.TTF' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="./css/style.css">
   </head>
-
 <body>
+
 	 <header>
       <div class="container">
         <div id="branding">
           <h1><span class="highlight">Aulas</span> <span id="bits">ONLINE</span></h1>
         </div>
-        <nav>
+         <nav>
           <ul>
             <li><a href="index.html">Home</a></li>
 			<li><a href="services.html">Aulas</a></li>
 			<li><a href="materiais.html">Materiais</a></li>
             <li ><a href="about.html">Sobre Nos</a></li>
-            <li ><a href="about.html">Aluno</a></li>
+            <li  class="current" ><a href="login.html">Aluno</a></li>
           </ul>
         </nav>
       </div>
@@ -34,15 +34,20 @@
 
 	include("conexao.php");
 
-	$sql = mysqli_query($conexao, "SELECT * FROM aluno");
+	$login = $_POST['login'];
+	$senha = $_POST['senha'];
 
-	while ($exibe = mysqli_fetch_assoc($sql)):
-			echo "<a href='form_edit_aluno.php?&id_aluno=". $exibe['id_aluno']. "'>Editar</a> | ";
-			echo "". $exibe["id_aluno"] . " | "."";
-			echo utf8_encode($exibe["login"] . " | "."");
-			echo "<a href='form_delet_aluno.php?&id_aluno=". $exibe['id_aluno']."'>Excluir/><br>";
-	endwhile;
+	if($sql = mysqli_query($conexao, "SELECT * FROM aluno  where login='$login' AND senha='$senha'")){
+		echo "<h1>Bem vindo, '$login'</h16>";
+	}else{
+		echo "Login e senha incorretos";
+	}
+
 	
-?>
-</body>
+
+	?>
+<footer>
+      <p>Aulas Online, Copyright &copy; 2017</p>
+    </footer>
+	</body>
 </html>
